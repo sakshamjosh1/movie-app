175df1257b1637f6dc02f533bff8d9f685341c9d
mohdd-anas <mohdanas48925@gmail.com>
2025-04-13 23:01:12 +0530

    Update script.js
    MINOR CHANGES

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const API_KEY = 'api_key=2e1ae992a1c327d7df0f4b34503760d4';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+const main = document.getElementById('main');
+const form = document.getElementById('form');
 
 getMovies(API_URL);
 
@@ -43,6 +46,3 @@ function getColor(vote) {
         return 'red'
     }
 }
-const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
-const main = document.getElementById('main');
-const form = document.getElementById('form');
