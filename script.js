2bba59e70ce02da932eb27e159e4f18492c199b6
mohdd-anas <mohdanas48925@gmail.com>
2025-04-13 23:00:42 +0530

    Update script.js
    MOVIE DATA CHANGES

--- script.js
+++ script.js
@@ -11,6 +11,38 @@ function getMovies(url) {
         showMovies(data.results);
     });
 }
+function showMovies(data) {
+    main.innerHTML = '';
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieE1 = document.createElement('div');
+        movieE1.classList.add('movie');
+        movieE1.innerHTML = `
+            <img src="${IMAGE_URL+poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieE1);
+    })
+}
+
+function getColor(vote) {
+    if (vote>=8){
+        return 'green'
+    }
+    else if (vote>=5){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 const main = document.getElementById('main');
 const form = document.getElementById('form');
