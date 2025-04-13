d0a157729c7fa5e8ef49a2ff41ce98d7abbe8047
Saksham Joshi <150180744+sakshamjosh1@users.noreply.github.com>
2025-04-13 11:04:33 +0530

    TMDB API to get movies

---
+++ script.js
@@ -0,0 +1,13 @@
+// TMDB
+
+const API_KEY = 'api_key=2e1ae992a1c327d7df0f4b34503760d4';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        showMovies(data.results);
+    });
+}
